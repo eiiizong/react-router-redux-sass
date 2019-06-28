31d3169440632fb2349a94d9a3ad8fd6819caa8b
eiiizong <eiiizong@163.com>
2019-06-28 18:23:25 +0800

    reduce

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import rootReducer from './reduces'
 
 import './index.scss';
 import App from './views/App';
 
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer);
 const rootEl = document.getElementById('root')
-ReactDOM.render(<App />, rootEl);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootEl
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reduces/index.js
@@ -0,0 +1,22 @@
+const types = {
+  CITY_NAME: 'CITY_NAME'
+}
+
+const chnageCityName = (state, action) => {
+  if (!state) {
+    return {
+      cityName: '成都市'
+    }
+  }
+  switch (action.type) {
+    case types.CITY_NAME:
+      return {
+        ...state,
+        cityName: action.cityName
+      }
+    default:
+      return state
+  }
+}
+
+export default chnageCityName
